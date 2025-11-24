1f699052899bb3a20d8e2e884a667a13a390fe6f
emarquezg-beep <emarquezg@gmail.com>
2025-11-24 17:32:36 -0500

    finzalizando thankyou

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/gracias" element={<ThankYou />} />
+          <Route path="/ThankYou" element={<ThankYou />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
